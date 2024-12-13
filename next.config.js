228b109de2afd9f03816f59b78ab3db785cce508
digitribesthlm <patrik@makeablesthlm.se>
2024-12-13 10:33:53 +0100

    new

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-const { DefinePlugin } = require('webpack');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_SCHEMA_API_URL: process.env.NEXT_PUBLIC_SCHEMA_API_URL,
     ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS,
     SCHEMA_DOMAIN: process.env.SCHEMA_DOMAIN
-  },
-  webpack: (config, { webpack }) => {
-    config.plugins = config.plugins || [];
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        '__SCHEMA_API_URL__': JSON.stringify(process.env.NEXT_PUBLIC_SCHEMA_API_URL)
-      })
-    );
-    return config;
   }
 };
 
