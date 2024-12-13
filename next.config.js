18cf78b8a0674103993c38cde4bef1e3fa6e7a95
digitribesthlm <patrik@makeablesthlm.se>
2024-12-13 11:00:35 +0100

    ok

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Note: we provide webpack above so you should not `require` it
+    config.resolve.modules.push(path.resolve('./'))
+    return config
+  }
 };
 
 module.exports = nextConfig;
