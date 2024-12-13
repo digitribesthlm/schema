e215507674154e8616c44abc52d1d3a518114e75
digitribesthlm <patrik@makeablesthlm.se>
2024-12-13 10:38:51 +0100

    su

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS,
-    SCHEMA_DOMAIN: process.env.SCHEMA_DOMAIN
-  }
-};
-
-module.exports = nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+// @ts-check
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const config = {
   reactStrictMode: true,
+  env: {
+    ALLOWED_DOMAINS: process.env.ALLOWED_DOMAINS,
+    SCHEMA_DOMAIN: process.env.SCHEMA_DOMAIN
+  }
 };
 
-export default nextConfig;
+export default config;
